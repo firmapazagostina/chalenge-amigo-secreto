588f602fd89a1dc477d32afa7f74dfa5fb28cc5b
Agostina Firma Paz <afirmapaz22@gmail.com>
2025-03-06 00:54:29 -0300

    codigo en Javascript

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    let ingreso = document.getElementById('amigo').value;
+    console.log(ingreso);
+    asignarTextoElemento('ul', ingreso)
+    amigos.push (ingreso);
+    console.log(amigos);
+    limpiarCaja();
+    return;
+}
+
+function crearLista(){
+    let ul = document.createElement('ul');
+    ul.textContent = agregarAmigo();
+    crearLista();
+
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
