4eb0e721b4fee0ea8dfc7f5073401154467537fa
Agostina Firma Paz <afirmapaz22@gmail.com>
2025-03-07 13:22:52 -0300

    Funciones

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
+let listaAmigos = [];
+const lista = document.getElementById('listaAmigos')
+let nombre = '';
 
 function asignarTextoElemento(elemento, texto){
     let elementoHtml = document.querySelector(elemento);
@@ -9,21 +11,17 @@ function asignarTextoElemento(elemento, texto){
 }
 
 function agregarAmigo(){
-    let ingreso = document.getElementById('amigo').value;
-    console.log(ingreso);
-    asignarTextoElemento('ul', ingreso)
-    amigos.push (ingreso);
-    console.log(amigos);
+    nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+    
+    
+    listaAmigos.push (nombre);
+    console.log(listaAmigos);
+    
     limpiarCaja();
     return;
 }
 
-function crearLista(){
-    let ul = document.createElement('ul');
-    ul.textContent = agregarAmigo();
-    crearLista();
-
-}
 
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
