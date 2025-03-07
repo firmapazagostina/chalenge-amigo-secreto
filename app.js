c345ac92eb27e377297d24ecfbb7c1f66ca23006
Agostina Firma Paz <afirmapaz22@gmail.com>
2025-03-07 19:11:48 -0300

    adicion de funciones

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [];
-const lista = document.getElementById('listaAmigos')
-let nombre = '';
 
-function asignarTextoElemento(elemento, texto){
-    let elementoHtml = document.querySelector(elemento);
-    elementoHtml.innerHTML = texto;
-    return;
-}
 
 function agregarAmigo(){
-    nombre = document.getElementById('amigo').value;
+    const nombre = document.getElementById('amigo').value;
     console.log(nombre);
+    if(nombre !== ''){
+        listaAmigos.push (nombre);
+        console.log(listaAmigos);
+    } else{
+        alert('ingrese un numero valido')
+    }
     
-    
-    listaAmigos.push (nombre);
-    console.log(listaAmigos);
-    
+    crearLista();
+
     limpiarCaja();
+
     return;
 }
 
+
+function crearLista(){
+    
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    listaAmigos.forEach( (nombre) => {
+        const li = document.createElement('li');
+        li.textContent = nombre;
+        lista.appendChild(li)
+    });
+    
+}
 
 function limpiarCaja(){
     document.querySelector('#amigo').value = '';
 }
+
+function sortearAmigo(){
+    if(listaAmigos.length < 2){
+        alert('se necesitan al menos 2 amigo para hacer el sorteo')
+    }
+    const sorteo = Math.floor(Math.random()*listaAmigos.length)
+    const amigoSorteado = listaAmigos[sorteo]
+    console.log(sorteo)
+    console.log(amigoSorteado)
+    
+    const resultado = document.getElementById('resultado')
+    resultado.innerHTML = `Felicidades! El amigo sorteado es: ${amigoSorteado}`;
+    
+    return;
+}
+
